35dfd2488faa0cd68cc35b6734a6ad7bde6195a9
Roman Alshehri <aalshehri08@hotmail.com>
2018-01-09 04:22:50 -0500

    Add reducers

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = (props) => (
+  <div>
+    <h3>Home page</h3>
+  </div>
+);
+
+export default HomePage;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+const data = {
+    name: "Roman"
+}
+
+export default combineReducers({
+    user: data
+});
